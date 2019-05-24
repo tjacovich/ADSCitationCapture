1e51638c458b1a81e3fc3e78b7eebfb1572c01df
Sergi Blanco-Cuaresma <marblestation@users.noreply.github.com>
2019-05-24 11:02:52 -0400

    Get canonical codes in chunks when list is larger than max

--- ADSCitationCapture/api.py
+++ ADSCitationCapture/api.py
@@ -39,46 +39,57 @@ def request_existing_citations(app, bibcode):
     existing_citation_bibcodes = [b['bibcode'] for b in existing_citation_bibcodes]
     return existing_citation_bibcodes
 
-def _get_canonical_bibcodes(app, bibcodes):
+def get_canonical_bibcodes(app, bibcodes, timeout=30):
     """
-    Convert input bibcodes into their canonical form if they exist
+    Convert input bibcodes into their canonical form if they exist, hence
+    the returned list can be smaller than the input bibcode list
     """
-    params = urllib.urlencode({
-                'fl': 'bibcode',
-                'q': '*:*',
-                'wt': 'json',
-                'fq':'{!bitset}',
-                'rows': len(bibcodes)
-            })
-    headers = {}
-    headers["Authorization"] = "Bearer:{}".format(app.conf['ADS_API_TOKEN'])
-    url = app.conf['ADS_API_URL']+"search/bigquery?"+params
-    data = "bibcode\n" + "\n".join(bibcodes)
-    r = requests.post(url, headers=headers, data=data)
-    return [d['bibcode'] for d in r.json().get('response', {}).get('docs', [])]
-
-def get_canonical_bibcodes(app, bibcodes):
-    """
-    Convert input bibcodes into their canonical form if they exist.
-    If the list of bibcodes is higher than the bigquery limit, it will
-    paginate through them.
-    """
-    start = 0
-    limit = 2000
-    n_bibcodes = len(bibcodes)
+    chunk_size = 2000 # Max number of records supported by bigquery
+    bibcodes_chunks = [bibcodes[i * chunk_size:(i + 1) * chunk_size] for i in range((len(bibcodes) + chunk_size - 1) / chunk_size )]
     canonical_bibcodes = []
-    while True:
-        canonical_bibcodes += _get_canonical_bibcodes(app, bibcodes[start:limit])
-        start += limit
-        if start > n_bibcodes:
-            break
+    total_n_chunks = len(bibcodes_chunks)
+    # Execute multiple requests to bigquery if the list of bibcodes is longer than the accepted maximum
+    for n_chunk, bibcodes_chunk in enumerate(bibcodes_chunks):
+        params = urllib.urlencode({
+                    'fl': 'bibcode',
+                    'q': '*:*',
+                    'wt': 'json',
+                    'fq':'{!bitset}',
+                    'rows': len(bibcodes_chunk),
+                })
+        headers = {}
+        headers["Authorization"] = "Bearer:{}".format(app.conf['ADS_API_TOKEN'])
+        headers["Content-Type"] = "big-query/csv"
+        url = app.conf['ADS_API_URL']+"search/bigquery?"+params
+        data = "bibcode\n" + "\n".join(bibcodes_chunk)
+        r_json = {}
+        try:
+            r = requests.post(url, headers=headers, data=data, timeout=timeout)
+        except:
+            logger.exception("BigQuery API request failed for bibcodes (chunk: %i/%i): %s", n_chunk, total_n_chunks, " ".join(bibcodes_chunk))
+            raise
+        else:
+            if not r.ok:
+                msg = "BigQuery API request with error code '{}' for bibcodes (chunk: {}/{}): {}".format(r.status_code, n_chunk, total_n_chunks, " ".join(bibcodes_chunk))
+                logger.error(msg)
+                raise Exception(msg)
+            else:
+                try:
+                    r_json = r.json()
+                except ValueError:
+                    msg = "No JSON object could be decoded from BigQuery API response when searching canonical bibcodes (chunk: {}/{}) for: {}".format(n_chunk, total_n_chunks, " ".join(bibcodes_chunk))
+                    logger.error(msg)
+                    raise Exception(msg)
+                else:
+                    for paper in r_json.get('response', {}).get('docs', []):
+                        canonical_bibcodes.append(paper['bibcode'])
     return canonical_bibcodes
 
-def get_canonical_bibcode(app, bibcode):
+def get_canonical_bibcode(app, bibcode, timeout=30):
     """
     Convert input bibcodes into their canonical form if they exist
     """
-    canonical = get_canonical_bibcodes(app, [bibcode])
+    canonical = get_canonical_bibcodes(app, [bibcode], timeout=timeout)
     if len(canonical) == 0:
         return None
     else:
