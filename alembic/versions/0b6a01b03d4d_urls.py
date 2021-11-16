1841ece262d1752e5398e9203319fd6f2acff3b8
tjacovich <tjacovich@cfa.harvard.edu>
2021-11-16 17:10:35 -0500

    Created alembic revision template

---
+++ alembic/versions/0b6a01b03d4d_urls.py
@@ -0,0 +1,24 @@
+"""urls
+
+Revision ID: 0b6a01b03d4d
+Revises: 5ba8c7af7acc
+Create Date: 2021-11-16 17:10:14.702803
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0b6a01b03d4d'
+down_revision = '5ba8c7af7acc'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
