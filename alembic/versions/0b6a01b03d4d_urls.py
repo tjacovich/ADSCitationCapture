48df7c77ee04218b9894c20da667d74e025cbb41
Taylor <tjacovich@cfa.harvard.edu>
2021-11-17 14:24:49 -0500

    Added initial downgrade function to alembic url version

--- alembic/versions/0b6a01b03d4d_urls.py
+++ alembic/versions/0b6a01b03d4d_urls.py
@@ -16,10 +16,32 @@ down_revision = '5ba8c7af7acc'
 branch_labels = None
 depends_on = None
 
-
 def upgrade():
+    #Add EMITTABLE to status ENUM types
     op.execute("ALTER TYPE target_status_type ADD VALUE 'EMITTABLE'")
     op.execute("ALTER TYPE citation_status_type ADD VALUE 'EMITTABLE'")
 
-
 def downgrade():
+    #Move expanded status types to old
+    op.execute("ALTER TYPE target_status_type RENAME TO  target_status_type_old")
+    op.execute("CREATE TYPE  target_status_type AS ENUM('REGISTERED', 'DELETED','UPDATED')")
+    #instantiate original status types
+    op.execute("ALTER TYPE citation_status_type RENAME TO  citation_status_type_old")
+    op.execute("CREATE TYPE  citation_status_type AS ENUM('REGISTERED', 'DELETED','UPDATED')")
+    
+    #DROP old ENUM types
+    op.execute("DROP TYPE target_status_type_old")
+    op.execute("DROP TYPE citation_status_type_old")
+    
+    #DROP expanded status columns
+    op.drop_column('citation_target_version','status')
+    op.drop_column('citation_target','status')    
+    op.drop_column('citation_version','status')
+    op.drop_column('citation','status')
+    
+    #ADD original status columns
+    op.add_column('citation_target',Column('status', postgresql.ENUM('REGISTERED', 'DELETED', 'DISCARDED', name='target_status_type'), nullable=True))
+    op.add_column('citation_target_version',Column('status', postgresql.ENUM('REGISTERED', 'DELETED', 'DISCARDED', name='target_status_type'), nullable=True))
+    op.add_column('citation_version',Column('status', postgresql.ENUM('REGISTERED', 'DELETED', 'DISCARDED', name='citation_status_type'), nullable=True))
+    op.add_column('citation',Column('status', postgresql.ENUM('REGISTERED', 'DELETED', 'DISCARDED', name='citation_status_type'), nullable=True))
+
