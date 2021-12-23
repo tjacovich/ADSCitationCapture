9fe905d49ae8ec9045589d61cfb4a476035e2e5b
Taylor <tjacovich@cfa.harvard.edu>
2021-12-23 14:40:54 -0500

    added initial alembic revision to include associated works in the public schema.

---
+++ alembic/versions/300bc74c63b5_associated_works.py
@@ -0,0 +1,24 @@
+"""associated works
+
+Revision ID: 300bc74c63b5
+Revises: 5ba8c7af7acc
+Create Date: 2021-12-23 14:39:51.018091
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '300bc74c63b5'
+down_revision = '5ba8c7af7acc'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
