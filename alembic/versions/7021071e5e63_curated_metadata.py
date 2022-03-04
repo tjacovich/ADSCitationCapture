645932ce85e3102cb1eefddd1c72d48dd14e301e
Taylor Jacovich <tjacovich@cfa.harvard.edu>
2022-03-04 09:43:54 -0500

    Testing methods for updating bibcodes from alembic upgrade

--- alembic/versions/7021071e5e63_curated_metadata.py
+++ alembic/versions/7021071e5e63_curated_metadata.py
@@ -7,7 +7,7 @@ Create Date: 2022-01-19 13:59:50.640443
 """
 from alembic import op
 import os
-from ADSCitationCapture import models
+from ADSCitationCapture import models, tasks
 import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 import adsputils
@@ -24,6 +24,7 @@ def upgrade():
     op.add_column('citation_target_version',sa.Column('curated_metadata', postgresql.JSONB(astext_type=sa.Text()), nullable=True))
     op.add_column('citation_target',sa.Column('bibcode', sa.Text(), nullable=True))
     op.add_column('citation_target_version',sa.Column('bibcode', sa.Text(), nullable=True))
+    tasks.task_maintenance_repopulate_bibcode_columns.delay()
     
 def downgrade():
     op.drop_column('citation_target','curated_metadata')
