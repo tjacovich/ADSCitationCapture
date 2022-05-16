d7d2871fec6da9a774c3ce6f82aad027f850e658
tjacovich <tjacovich@cfa.harvard.edu>
2022-05-16 10:42:56 -0400

    Merged alembic heads between doi sanitization and master branch.

---
+++ alembic/versions/cd06eee98a8a_doi_sanitize_and_associated.py
@@ -0,0 +1,24 @@
+"""doi_sanitize_and_associated
+
+Revision ID: cd06eee98a8a
+Revises: 8e83568feb1d, 98cb3e36b2da
+Create Date: 2022-05-16 10:35:34.117170
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cd06eee98a8a'
+down_revision = ('8e83568feb1d', '98cb3e36b2da')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
